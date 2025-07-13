0a2455dda5594b8fbb3f7523cb743333b9566c0b
zshaffer1213 <zshaff1213@gmail.com>
2025-07-13 12:10:25 -0400

    set up

---
+++ habit-tracker/src/App.jsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+
+export default function App() {
+
+  const [habits, setHabits] = useState([])
+  const [newHabitName, setNewHabitName] = useState('')
+
+  
+
+}
